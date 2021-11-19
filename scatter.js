50ec6776d3412eb7467ba17214fa631d4585eac2
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2021-11-19 11:58:49 +0000

    Make outside circle pixels transparent

--- scatter.js
+++ scatter.js
@@ -10553,11 +10553,26 @@
             
             float outside_circle = step(radius, length_from_origin); // return 1 if it is outside the circle
             float inside_circle = 1.0 - outside_circle; // will return 1.0 if it is inside the circle
-            gl_FragColor = vec4(0.0, 0.0, inside_circle, 1.0);
+            float alpha = inside_circle; // if point is inside circle we want alpha to be 1, if it is outside circle we want it to be zero (same as inside_circle)
+            gl_FragColor = vec4(0.0, 0.0, inside_circle, alpha);
     
         }
     
     `, 
+	    blend:  { // this object is copied pasted, this sets up behaviour between
+	    // source and destination colors. it makes transparency possible
+	        enable: true,
+	        func: {
+	            srcRGB: "src alpha",
+	            dstRGB: "one minus src alpha",
+	            srcAlpha: "src alpha",
+	            dstAlpha: "one minus src alpha"
+	        },
+	        equation: {
+	            rgb: "add",
+	            alpha: "add"
+	        }
+	    },
 
 	});
 
