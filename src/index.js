a15b3e926b928f73b7a04779b13ec1552e6e39d1
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2021-11-19 12:38:28 +0000

    Use gl-matrix to gl_position to correct for aspect ratio

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 const REGL = require("regl");
+const { mat4 } = require("gl-matrix"); // has bunch of stock standard mat's for working with matrices
+// we are using it for transformations to alter the canvas proportions so that a square is a square
+// mat4 is a matrix with 4 rows and 4 columns, (so 16 numbers) we are using a mat4 because we working with a vec4 (gl_position)
+// we want to transform the current gl_position into a new gl_position that is correctly proportioned.
 
 const regl = REGL({});
 
-
+const projection_matrix = mat4.create(); // this returns mat4 identity (i.e. it just returns the same thing it gets in)
 
 const drawPoints = regl({
     count: 6, // amount of points we are going to draw.
     attributes: {
         position: [[-1, -1], [1, -1], [1, 1], [1, 1], [-1, 1], [-1, -1]]
     },
-    uniforms: {},
+    uniforms: {
+        projection_matrix: ()=> projection_matrix
+    },
     vert: `
         precision mediump float;
         attribute vec2 position;
         varying vec2 v_position;
+        uniform mat4 projection_matrix;
 
         void main(){
             v_position = position;
             gl_Position = vec4(position, 0.0, 1.0);
+            gl_Position = projection_matrix * gl_Position; // this is the gl position transformed by the mat4
         }
     `,
     frag: `
@@ -57,7 +65,12 @@ const drawPoints = regl({
 function render(){
 
     regl.clear({ color: [0.2, 0.5, 0.4, 1.0] });
-
+    mat4.identity(projection_matrix); // this resets the matrix to the original mat4 identity matrix,
+    // so if we need to update the transofmraiton (e.g. for screen resize) it will just do the right amount (cos its cummulative)
+    mat4.scale(projection_matrix, projection_matrix, [0.5, 0.5, 1.0]); // projection_matrix is passed in twice because its both the matrix we are reading from and the one we are writing to
+    const ratio = window.innerWidth/ window.innerHeight;
+    // mat4.ortho(transformation_matrix, left, right, up, down, near, far) // defining a cube for the edges of the scene
+    mat4.ortho(projection_matrix, -ratio , ratio, 1.0, -1.0, -1.0, 1.0);
     drawPoints();
 
 }
