7b35908807e1a573ffd53a5f6f1f2f927c090da7
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2021-11-19 14:42:42 +0000

    Use instancing to draw multiple circles

--- src/index.js
+++ src/index.js
@@ -4,15 +4,29 @@ const { mat4 } = require("gl-matrix"); // has bunch of stock standard mat's for 
 // mat4 is a matrix with 4 rows and 4 columns, (so 16 numbers) we are using a mat4 because we working with a vec4 (gl_position)
 // we want to transform the current gl_position into a new gl_position that is correctly proportioned.
 
-const regl = REGL({});
+const regl = REGL({
+    extensions: ["ANGLE_instanced_arrays"] // this makes the instancing work
+});
 
 
 const projection_matrix = mat4.create(); // this returns mat4 identity (i.e. it just returns the same thing it gets in)
 
 const drawPoints = regl({
+    instances: 2, // this says we want two instances (i.e. two circles)
     count: 6, // amount of points we are going to draw.
     attributes: {
-        position: [[-1, -1], [1, -1], [1, 1], [1, 1], [-1, 1], [-1, -1]]
+    // attributes can be called anything, but the options they take are all the same (they have buffer and divisor). If they dont have a divisor
+    // then you can do it in an array e.g. position below:
+        position: [[-1, -1], [1, -1], [1, 1], [1, 1], [-1, 1], [-1, -1]], // this is shorthand for doing an object with buffer without a divisor
+        // (regular attributes have a divisor of zero, only instance attributes have a divisor of one)
+        color: {
+            buffer: [[1, 0, 0],[ 0, 0, 1]], // red and blue
+            divisor: 1 // this turns on instancing (i.e. use the following code as a template that will be called many times)
+        },
+        offset: {
+            buffer: [[-1, 0], [1, 0]],
+            divisor: 1
+        }
     },
     uniforms: {
         projection_matrix: ()=> projection_matrix
@@ -22,10 +36,15 @@ const drawPoints = regl({
         attribute vec2 position;
         varying vec2 v_position;
         uniform mat4 projection_matrix;
+        attribute vec3 color;
+        varying vec3 v_color;
+        attribute vec2 offset;
+
 
         void main(){
+            v_color = color;
             v_position = position;
-            gl_Position = vec4(position, 0.0, 1.0);
+            gl_Position = vec4(position + offset, 0.0, 1.0);
             gl_Position = projection_matrix * gl_Position; // this is the gl position transformed by the mat4
         }
     `,
@@ -33,6 +52,7 @@ const drawPoints = regl({
         precision mediump float;
         varying vec2 v_position;
         vec3 sienna = vec3(0.725, 0.345, 0.207);
+        varying vec3 v_color;
 
         void main(){
             // length(v_position) = length from origin
@@ -42,7 +62,7 @@ const drawPoints = regl({
             float outside_circle = step(radius, length_from_origin); // return 1 if it is outside the circle
             float inside_circle = 1.0 - outside_circle; // will return 1.0 if it is inside the circle
             float alpha = inside_circle; // if point is inside circle we want alpha to be 1, if it is outside circle we want it to be zero (same as inside_circle)
-            gl_FragColor = vec4(sienna, alpha);
+            gl_FragColor = vec4(v_color, alpha);
 
         }
 
